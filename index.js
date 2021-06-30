cb571c584857e33a7d348468b223a2d5d4df88ae
Thinh Nguyen <t9ngdu00@students.oamk.fi>
2021-06-30 13:03:23 +0300

    menu system with api(GET, POST, DELETE, PUT)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcryptjs');
 const Strategy = require('passport-http').BasicStrategy;
 const passport = require('passport');
 const cors = require('cors');
-const menuComponent = require('./components/menu')
+// const menuComponent = require('./components/menu')
 const db = require('./db');
 const app = express();
 const port = 4000;
@@ -14,7 +14,7 @@ const saltRounds = 4;
 
 app.use(express.json());
 app.use(cors())
-app.use('/menu', menuComponent);
+// app.use('/menu', menuComponent);
 
 
 
@@ -79,6 +79,84 @@ app.post('/users', (req, res) => {
     res.sendStatus(404);
   }
 })
+
+
+let menu = [{
+  id : uuidv4(),
+  name : "Caffè Americano",  
+  description: "Espresso shots topped with hot water create a light layer of crema culminating in this wonderfully rich cup with depth and nuance. Pro Tip: For an additional boost, ask your barista to try this with an extra shot.",
+  nutritioninformation : "Nutrition information is calculated based on our standard recipes. Only changing drink size will update this information. Other customizations will not.", 
+  size : "16", 
+  calories : "15",  
+  fat : "0",   
+  cholesterol : "0",
+  carbohydrates : "2", 
+  protein : "1", 
+  caffeine  : "225",
+  ingredients : "Water, Brewed Espresso", 
+  price : "2", 
+  img : "img37.jpg" 
+}];
+
+app.get('/menu', (req, res) => {
+  res.json(menu);
+})
+
+// Read info on single drink
+app.get('/menu/:id', (req, res) => {
+  const results = menu.find(d => d.id === req.params.id)
+
+  res.json(results);
+})
+
+//Create a new drink
+app.post('/menu', (req, res) => {
+  console.log('New drink');
+  console.log(req.body);
+  menu.push({ 
+    id: uuidv4(),
+    name: req.body.name,
+    description: req.body.description,
+    nutritioninformation: req.body.nutritioninformation,
+    size: req.body.size,
+    calories: req.body.calories,
+    fat: req.body.fat,
+    cholesterol: req.body.cholesterol,
+    carbohydrates: req.body.carbohydrates,
+    protein: req.body.protein,
+    caffeine: req.body.caffeine,
+    ingredients: req.body.ingredients,
+    price: req.body.price,
+    img: req.body.img
+  })
+  console.log('Drink name ' + req.body.name);
+  res.send('CREATING new drink');
+})
+
+//Delete a drink
+app.delete('/menu/:id', (req, res) => {
+
+  const result = menu.findIndex(d => d.id === req.params.id);
+  if(result !== -1) {
+    menu.splice(result, 1);
+    res.send('Delete succeeded');
+  } else {
+    res.send('no such drink found');
+  }
+  res.send('DELETE drink with id' + req.params.id);
+})
+
+//Delete all drinks
+app.delete('/menu', (req, res) => {
+  res.send('DELETE all drinks');
+})
+
+//Modify a drink
+app.put('/menu/:id', (req, res) => {
+  res.send('Modifying drink')
+})
+
+
 
 Promise.all(
   [
